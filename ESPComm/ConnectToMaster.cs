48950310b006f01019b1e1013e6ad01b966bb0d1
Calin Peters <calinpeters98@hotmail.nl>
2019-12-17 12:51:23 +0100

    Full app this time

---
+++ ESPComm/ConnectToMaster.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ESPComm
+{
+    class ConnectToMaster
+    {
+        private int _baudRate;
+        private string _portName;
+        private SerialPort port;
+
+
+        public ConnectToMaster()
+        {
+
+        }
+
+        
+        public ConnectToMaster(int baud, string portName)
+        {
+            this._portName = portName;
+            this._baudRate = baud;
+        }
+
+        public bool OpenConnection(int baud, string portName)
+        {
+            this._baudRate = baud;
+            this._portName = portName;
+
+            try
+            {
+                port = new SerialPort {BaudRate = this._baudRate, PortName = this._portName};
+                port.Open();
+                return true;
+
+            } catch (Exception e)
+            {
+                e = new Exception("Can't open the serial connection to Master.\n");
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+            
+
+        }
+
+        public bool Write(string instruction)
+        {
+            try
+            {
+                port.Write(instruction);
+                System.Threading.Thread.Sleep(500);
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                e = new Exception("Can't read serial connection.\n");
+                Console.WriteLine(e.ToString());
+                return false;
+            }
+        }
+
+        public int BaudRate
+        {
+            get => _baudRate;
+            set => _baudRate = value;
+        }
+
+        public string PortName
+        {
+            get => _portName; 
+            set => _portName = value;
+        }
+
+
+    }
+}
---
+++ ESPComm/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Xml;
+
+namespace ESPComm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var con = new ConnectToMaster {BaudRate = 9600, PortName = "COM5"};
+            con.OpenConnection(con.BaudRate, con.PortName);
+
+            //Testscript vanaf hier.
+            bool inf = true;
+
+            Console.WriteLine("\nMaster aangesloten op " + con.PortName);
+
+            while (inf)
+            {
+
+
+                Console.WriteLine("\nMaak een keuze:\n1. Licht aansturen.\n2. Deur sluiten.\n");
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    Console.WriteLine("\nWat wilt u met het licht doen?\n1. Licht aanzetten\n2.Licht uitzetten\n");
+                    string incoming = Console.ReadLine();
+                    switch (incoming)
+                    {
+                        case "1":
+                            con.Write("1");
+                            Console.WriteLine("Lamp aan.\n");
+                            break;
+
+                        case "2":
+                            con.Write("0");
+                            Console.WriteLine("Lamp uit.\n");
+                            break;
+                        default:
+                            Console.WriteLine("Maak een keuze.\n");
+                            break;
+                    }
+
+
+
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("\nWat wilt u met het slot doen?\n1. Openen\n2.Sluiten\n");
+                    string incoming = Console.ReadLine();
+                    switch (incoming)
+                    {
+                        case "1":
+                            con.Write("1");
+                            Console.WriteLine("Slot geopend.\n");
+                            break;
+
+                        case "2":
+                            con.Write("0");
+                            Console.WriteLine("Slot gesloten.\n");
+                            break;
+                        default:
+                            Console.WriteLine("Maak een keuze.\n");
+                            break;
+                    }
+                }
+
+            }
+        }
+    }
+}
